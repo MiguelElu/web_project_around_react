c116cd2d94398553aca695ca577eb880617ff4e7
MiguelElu <miguel.elu.lanzagorta@gmail.com>
2025-12-24 15:39:19 -0600

    Fix names and locations of files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
-import "../blocks/page.css";
-import "../blocks/header.css";
-import "../blocks/content.css";
-import "../blocks/footer.css";
-import "../blocks/form.css";
-import "../blocks/fullview.css";
-import "../blocks/confirmation.css";
-import "../vendor/fonts.css";
-import "../vendor/normalize.css";
-import API from "./utils/api";
-import Header from "./components/header";
-import Body from "./components/body";
-import Footer from "./components/footer";
-import Popup from "./components/popup";
-import NewCard from "./components/form/card/card";
-import UserInfo from "./components/form/user_info/user_info";
-import ChangeAvatar from "./components/form/avatar/avatar";
+import "../../blocks/page.css";
+import "../../blocks/header.css";
+import "../../blocks/content.css";
+import "../../blocks/footer.css";
+import "../../blocks/form.css";
+import "../../blocks/fullview.css";
+import "../../blocks/confirmation.css";
+import "../../vendor/fonts.css";
+import "../../vendor/normalize.css";
+import API from "../utils/api";
+import Header from "./header";
+import Body from "./Main";
+import Footer from "./footer";
+import Popup from "./popup";
+import NewCard from "./form/card/card";
+import UserInfo from "./form/user_info/EditProfile";
+import ChangeAvatar from "./form/avatar/EditAvatar";
 
-import CurrentUserContext from "./contexts/CurrentUserContext";
+import CurrentUserContext from "../contexts/CurrentUserContext";
 
 function App() {
   const [currentUser, setCurrentUser] = useState({});
